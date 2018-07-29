e05b5e5c6eb58cc7fa90405929357151b94a2fcd
Diego Rodriguez <diefrodriguezcha@unal.edu.co>
2018-07-29 00:24:47 -0500

    Added favicon, started rec-map v1

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ function onGoogleMapResponse() {
     }
     map = new google.maps.Map(document.getElementById("mapContainer"), mapInitialSetup);
 }
+
+
+
+/*  Obtiene escuelas del dataset y las pone en el mapa */
+function getSchools() {
+
+    $.ajax({
+        url: "http://datosabiertos.bogota.gov.co/api/action/datastore_search?resource_id=8e3cdb48-266f-45bd-8006-21040db5b876",
+        data: "data",
+        type: "GET",
+        dataType: "jsonp",        
+        success: function(result) {
+            console.log(result);
+        }
+    })
+}
+
+$(document).ready(function() {
+    getSchools();
+});
