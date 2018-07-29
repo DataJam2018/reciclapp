8ac740e34fe37f0b3dde2edbf9cbfbafd3ad9fda
Diego Rodriguez <diefrodriguezcha@unal.edu.co>
2018-07-29 03:02:17 -0500

    Added infowindows for each marker in map.

--- index.js
+++ index.js
@@ -17,12 +17,26 @@ function getSchools() {
 
     $.getJSON("institucionesedsuperior.json", function(data){
         console.log(data);
-        var marker;
+        var marker, infoWindow, content;
         for(var i = 0; i < data.length ; i++) {
             marker = new google.maps.Marker({
                 position: new google.maps.LatLng(data[i].coord_y, data[i].coord_x),
                 map: map
             });
+
+            content = "<div style='font-weight:bold;'>" + data[i].Institución + "<br>"+ data[i].Dirección+"</div>";
+            infoWindow = new google.maps.InfoWindow({
+                content: content
+              });
+              listenMarker(marker);
+              function listenMarker(marker){
+                google.maps.event.addListener(marker, 'click', (function(marker, content, infoWindow){
+                  return function(){
+                    infoWindow.setContent(content);
+                    infoWindow.open(map, marker);
+                  };
+                })(marker, content, infoWindow));      
+              }
             schools.push(marker);
         }
 
